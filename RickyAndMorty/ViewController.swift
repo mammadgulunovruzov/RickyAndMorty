4ed05bb1e336be38e99a075caf3d8d2be42bd8e5
Mammadgulu Novruzov <mnovruzov2021@ada.edu.az>
2025-04-15 13:13:08 +0200

    Initial Commit

---
+++ RickyAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickyAndMorty
+//
+//  Created by Mammadgulu Novruzov on 15.04.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
