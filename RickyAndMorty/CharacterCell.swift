f04c51774c7c1fea432096d2de4d8f27acbb1e5d
Mammadgulu Novruzov <mnovruzov2021@ada.edu.az>
2025-04-16 01:03:20 +0200

    Rick and Morty Application Submission

---
+++ RickyAndMorty/CharacterCell.swift
@@ -0,0 +1,23 @@
+//
+//  CharacterCell.swift
+//  RickyAndMorty
+//
+//  Created by Mammadgulu Novruzov on 15.04.25.
+//
+
+import UIKit
+
+class CharacterCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ RickyAndMorty/Characters.swift
@@ -0,0 +1,8 @@
+//
+//  Characters.swift
+//  RickyAndMorty
+//
+//  Created by Mammadgulu Novruzov on 15.04.25.
+//
+
+import Foundation
