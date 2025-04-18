0c1456190d974a05976f1d8ace6aee9703399445
Mammadgulu Novruzov <mnovruzov2021@ada.edu.az>
2025-04-18 13:57:25 +0200

    File separating, Text and cell colors in dark and light modes, removing sdwebimage pod

---
+++ RickAndMorty/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  RickAndMorty
+//
+//  Created by Mammadgulu Novruzov on 18.04.25.
+//
+
+import Foundation
---
+++ RickAndMorty/UIImageView+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIImageView+Extension.swift
+//  RickAndMorty
+//
+//  Created by Mammadgulu Novruzov on 18.04.25.
+//
+
+import Foundation
